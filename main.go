56dee2c70e9dfbb55326ef4ec8f108428c6cee3d
thanadon.s <thanadonsaojarkaval@gmail.com>
2025-05-13 17:18:52 +0700

    - initial mariadb container

--- main.go
+++ main.go
@@ -1,18 +1,40 @@
 package main
 
 import (
-	"time"
+	"fmt"
+	"goredis/repositories"
 
-	"github.com/gofiber/fiber/v3"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
 )
 
 func main() {
-	app := fiber.New()
+	db := initDatabase()
 
-	app.Get("/hello", func(c fiber.Ctx) error {
-		time.Sleep(time.Millisecond * 10)
-		return c.SendString("Hello")
-	})
+	productRepo := repositories.NewProductRepositoryDB(db)
 
-	app.Listen(":8000")
+	products, err := productRepo.GetProducts()
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Println(products)
+	// app := fiber.New()
+
+	// app.Get("/hello", func(c fiber.Ctx) error {
+	// 	// time.Sleep(time.Millisecond * 10)
+	// 	return c.SendString("Hello")
+	// })
+
+	// app.Listen(":8000")
+}
+
+func initDatabase() *gorm.DB {
+	dial := mysql.Open("root:P@ssw0rd@tcp(localhost:3306)/goredis")
+	db, err := gorm.Open(dial, &gorm.Config{})
+	if err != nil {
+		panic(err)
+	}
+
+	return db
 }
